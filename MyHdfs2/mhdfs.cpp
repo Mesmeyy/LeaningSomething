cc9a06f7f236874821a87ffef01763dd38507c74
zhuziyu <mesmeyy@sina.com>
2019-12-04 13:28:53 +0800

    MyHDFS

--- MyHdfs2/mhdfs.cpp
+++ MyHdfs2/mhdfs.cpp
@@ -23,13 +23,16 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <vector>
+#include <queue>
 using namespace std;
 
 struct hdfsBuilder *pbld = NULL;
+std::queue<struct hdfsStreamBuilder*> queue_hdfsstreambuilder;
 hdfsFS my_hdfsfs;
+hdfsFile my_hdfsfile;
 std::string masterip = "172.19.0.207";
 int masterport = 9000;
-
+std::string FilePath = "/data/pictures";
 int filenum;
 
 std::vector<string> filepath;
@@ -61,17 +64,52 @@ bool ReadMyHdfs()
 	cout<<":Connect to hdfs success,cur time:"<<time(NULL)<<endl;
 	hdfsFileInfo *fileinfo;
 	std::string dicpath = "/data/pictures";
-	if (NULL != (fileinfo = hdfsListDirectory(my_hdfsfs, "/data/pictures", &filenum))){
+	if (NULL != (fileinfo = hdfsListDirectory(my_hdfsfs, FilePath.c_str(), &filenum))){
 		for (int i = 0; i < filenum; i++,fileinfo++){
+            std::cout << "*********************************"<<endl;
 			string filename = fileinfo->mName;
 			cout << "filename = " << filename<<endl;
+			int portindex = filename.rfind(":");
+			portindex += 5;
+			string filefullpath = filename.substr(portindex);
+			filepath.push_back(filefullpath);
+			std::cout << "file_full_path = " << filefullpath<<endl;
+
+            /*
+			struct hdfsStreamBuilder *temp;
+            temp = hdfsStreamBuilderAlloc(my_hdfsfs,filefullpath.c_str(),O_RDONLY);
+            if(temp != NULL){
+            	queue_hdfsstreambuilder.push(temp);
+            }
+
+            hdfsFile my_hdfsfile = hdfsStreamBuilderBuild(temp);
+            */
+
+            //get default block size
+            //tOffset defaultsize = hdfsGetDefaultBlockSizeAtPath(my_hdfsfs,filefullpath.c_str());
+           //std::cout << "dedaultsize = " << defaultsize<<endl;
+
+            std::cout <<"file size = "<< fileinfo->mSize<<endl;
+            std::cout << "file block size = "<<fileinfo->mBlockSize<<endl;
+
+            char*** filehost = hdfsGetHosts(my_hdfsfs, filefullpath.c_str(),0, fileinfo->mBlockSize);
+             if(filehost != 0){
+            	 if(filehost[0] != NULL) cout << filehost[0][1]<<endl;
+             }
+            /*
 			hdfsFile hdfstmpfile = hdfsOpenFile(my_hdfsfs, filename.c_str(), O_RDONLY, 0, 2, 0);
 		    if(!hdfstmpfile){
 		    	cout << "file("<<filename<<") open fail..."<<endl;
-		    }
-		    //Size result = hdfsRead(my_, readFile, (void*)buffer, bufferSize);
+		    }*/
+		    /*read file in while*/
+		    /*char buffer[1024];
+		    int length = buffer[1024];
+		    tSize result = hdfsRead(my_hdfsfs, my_hdfsfile, (void*)buffer, length);
+		    //hdfsseek
+		    if(result <-1) cout << "Read fial..."<< endl;
 		    cout << "file("<<filename<<") open success..."<<endl;
-
+		    */
+            cout << "*************************************"<<endl;
 		}
 	}
 	if ( 0 != hdfsDisconnect(my_hdfsfs) ){
