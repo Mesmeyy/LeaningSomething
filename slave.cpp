74526d3fd7c0fb25ed0b5fc340b62691f3938a20
zhuziyu1157817544 <mesmeyy@sina.com>
2020-02-29 08:56:56 +0800

    分布式kmeans算法(slave-cpu)

---
+++ slave.cpp
@@ -0,0 +1,58 @@
+/*************************************************************************
+	> File Name: slave.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 28 Feb 2020 07:45:11 PM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+#include <string.h>
+#include <fstream>
+#include <unistd.h>
+
+using namespace std;
+
+const double INF= 1e20;
+const int MAXD = 1000;
+const int MAXN = 1000;
+const int MAXC = 50;
+
+struct aCluster
+{
+    double Center[MAXD];//类的中心
+    int Number;//类中包含的样本point数目
+    int Member[MAXN];//类中包含的样本point的index
+    //double Err;//我想同意使用一个err
+};
+class S_Kmeans{
+    private:
+    double Point[MAXN][MAXD];//slave必须要保存所有的数值
+    struct aCluster Slave_acluster;//该slave独有的一个cluster
+    int Point_Num;//样本数
+    int Point_Dimension;//样本属性维度
+    int Cluster_Num;
+    double Tempcenter[MAXD];
+
+    public:
+    bool ReadData();
+};
+bool S_Kmeans::ReadData()
+{
+    ifstream infile;
+    infile.open("data.txt");
+    infile >> Point_Num;
+    infile >> Point_Dimension;
+    infile >> Cluster_Num;
+    for(int i = 0;i < Point_Num;i++){
+        for(int j = 0;j < Point_Dimension;j++){
+            infile >> Point[i][j];
+        }
+    }
+    infile.close();
+    std::cout << "read tempdata for this slave is ok..."<<std::endl;
+    //slave不需要把数据写入其中，但是他需要从属于它的tempdata里面读取center;
+}
